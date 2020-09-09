4ce005ac19a6c4ffe76f898f25fed3bb2e180b22
Anya <likamoon@list.ru>
2020-09-09 22:23:29 +0300

    Блоки в новом дизайне + новые inner

--- public/dist/timer.js
+++ public/dist/timer.js
@@ -13,7 +13,7 @@ function ready() {
 
           var blockDaysText = document.querySelector('#clock-name-days');
 
-          $('#clock').countdown('2020/08/31', function(event) {
+          $('#clock').countdown('2020/09/30', function(event) {
             days = event.strftime('%D');
             hours = event.strftime('%H');
             minutes = event.strftime('%M');
@@ -48,3 +48,54 @@ function ready() {
         }
       }
       document.addEventListener("DOMContentLoaded", ready);
+
+      function ready2() {
+        var clockInPage = document.querySelector("#new-clock");
+        if(clockInPage) {
+          var days;
+          var hours;
+          var minutes;
+          var seconds;
+
+          var daysBlock = document.querySelector('#new-clock-days');
+          var hoursBlock = document.querySelector('#new-clock-hours');
+          var minutesBlock = document.querySelector('#new-clock-minutes');
+          var secondsBlock = document.querySelector('#new-clock-seconds');
+
+          var blockDaysText = document.querySelector('#new-clock-name-days');
+
+          $('#new-clock').countdown('2020/09/30', function(event) {
+            days = event.strftime('%D');
+            hours = event.strftime('%H');
+            minutes = event.strftime('%M');
+            seconds = event.strftime('%S');
+
+            var daysNum = +days;            
+            if((daysNum > 20) || (days <5)) {
+              var number = daysNum%10;
+              if((number === 4) || (number === 3) || (number === 2)) {
+                blockDaysText.innerText = 'Дня';
+              } else if (number === 1) {
+                blockDaysText.innerText = 'День';
+              } else {
+                blockDaysText.innerText = 'Дней';
+              }
+            } else if ( (daysNum <= 20) && (daysNum >4 ) ) {
+              blockDaysText.innerText = 'Дней';
+            } 
+            days = "<span>" + days[0] + "</span><span>" + days[1] + "</span>";
+            hours = "<span>" + hours[0] + "</span><span>" + hours[1] + "</span>";
+            minutes = "<span>" + minutes[0] + "</span><span>" + minutes[1] + "</span>";
+            seconds = "<span>" + seconds[0] + "</span><span>" + seconds[1] + "</span>";
+
+            daysBlock.innerHTML = days;
+            hoursBlock.innerHTML = hours;
+            minutesBlock.innerHTML = minutes;
+            secondsBlock.innerHTML = seconds;
+
+                  // $(this).html(event.strftime('%D:%H:%M:%S'));
+                });
+
+        }
+      }
+      document.addEventListener("DOMContentLoaded", ready2);
